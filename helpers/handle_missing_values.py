0fed70db5e03a4e558e2f36e65c69b2a8fa3ddde
Kislaya N <kislaya345@gmail.com>
2026-04-27 17:37:34 +0530

    feat: added a helper method to impute nan values

---
+++ helpers/handle_missing_values.py
@@ -0,0 +1,13 @@
+from sklearn.impute import KNNImputer
+
+class Imputer:
+    def __init__(self):
+        self.impute_method = KNNImputer(n_neighbors=5, weights='distance')
+        self.transformed_data = None
+        
+    def transform(self, target):
+        """The value you want to impute is target"""
+        
+        target = self.impute_method.fit_transform(target)
+        self.transformed_data = target
+        return self.transformed_data
