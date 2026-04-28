d95901f546b7cb92f3132bb22c35d7bb004df972
Kislaya N <kislaya345@gmail.com>
2026-04-28 13:55:30 +0530

    refactor: fixed original dataset update issue

--- feature_scaling/preprocessing.py
+++ feature_scaling/preprocessing.py
@@ -6,12 +6,15 @@ class Preprocess:
     def __init__(self, perform_scaling=True):
         self.perform_scaling = perform_scaling
         self.scaler = StandardScaler()
+        self.X_original = None
         
     def fit(self, X):
         self.scalable_cols = X.select_dtypes(include=[np.number]).columns
         self.scaler.fit(X[self.scalable_cols])
     
     def transform(self, X):
+        self.X_original = X.copy()
+        
         if self.perform_scaling:
             if not hasattr(self, 'scalable_cols'):
                 return RuntimeError(".fit() should be called first")
