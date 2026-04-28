15a43190ff89c9234e282e7b9574e4bf05303ab1
Kislaya N <kislaya345@gmail.com>
2026-04-28 13:21:32 +0530

    feat: added a feature for Linear regression

---
+++ models/LR.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LinearRegression
+
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, fit_intercept=True):
+        self.model = LinearRegression(fit_intercept=fit_intercept)
+        
+    def fit(self, X, y):
+        X_val = X.values if hasattr(X, 'values') else X
+        y_val = y.values if hasattr(y, 'values') else y
+        
+        self.model.fit(X, y)
+        
+    def predict(self, X):
+        predictions = self.model.predict(X)
+        return predictions
