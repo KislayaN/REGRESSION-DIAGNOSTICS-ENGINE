4c92180994f89251671c8d87e0a9d7b6800444d0
Kislaya N <kislaya345@gmail.com>
2026-04-28 15:23:03 +0530

    refactor: fixed parameter issues

--- train_test_split/split.py
+++ train_test_split/split.py
@@ -2,9 +2,12 @@ from sklearn.model_selection import train_test_split
 
 class Splitter:
     def __init__(self):
-        self.splitter = train_test_split()
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
         
     def split(self, X, y):
-        self.X_train, self.X_test, self.y_train, self.y_test = self.splitter(X, y, random_state=43, test_size=0.2)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, random_state=43, test_size=0.2)
         
         return self.X_train, self.X_test, self.y_train, self.y_test
