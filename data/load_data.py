0d953d8fc0ce627a38317f1ca26f6f1109891012
Kislaya N <kislaya345@gmail.com>
2026-04-28 09:48:59 +0530

    refactor: load_data fixed

--- data/load_data.py
+++ data/load_data.py
@@ -1,51 +1,26 @@
 import numpy as np
-
-import sys
-import os
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-project_root = os.path.exists(os.path.join(current_dir, ".."))
-
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-    
-from helpers.handle_missing_values import Imputer
+import pandas as pd
 
 class Load_data:
-    is_1D = False
-    contains_same_samples = False
-    is_numeric = False
-    validated = False
-    
-    def __init__(self, data=None, target=None):
-        self.dataset = data
+    def __init__(self):
+        self.data_loaded = False
+        
+    def load_data(self, data, target, path):
+        if data is not None:
+            dataframe = data.copy()
+        elif path is not None:
+            dataframe = pd.read_csv(path)
+        else:
+            raise ValueError("Provide data or path")
+        
+        if target not in dataframe:
+            raise ValueError("Target not found in provided dataframe")
+        
+        self.dataframe = dataframe
+        self.path = path
         self.target = target
-        self.imputer = Imputer()
         
-    def validate_target(self):
-        # validation 1: target vector must be a 1-Dimensional array
-        if self.target.ndim == 1:
-            self.is_1D = True    
-            
-        # validation 2: X and y should have same number of samples
-        if self.dataset.shape[1] == self.target.shape[1]:
-            self.contains_same_samples = True
-            
-        # validation 3: y should be numeric
-        if np.issubdtype(self.target, np.number):
-            self.is_numeric = True
+        self.y = self.dataframe[target].values
+        self.X = self.dataframe.drop(columns=[target]).values
         
-        # validation 4: should not have missing values, if present (handle them)
-        if self.target.isna().any().any():
-            target = self.imputer.transform(self.target)
-            
-        self.validated = True
-        return self.validated
-            
-    def get_data(self):
-        """Returns a tuple of dataset and target"""
-        
-        if self.validated:
-            return self.dataset, self.target
-        
-        return "The target need to validate, call .validate_target() first"
+        self.data_loaded = True
