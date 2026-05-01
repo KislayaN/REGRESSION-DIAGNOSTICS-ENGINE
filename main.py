574aa28be32f45ece468c060342965d2f6573a2a
Kislaya N <kislaya345@gmail.com>
2026-05-01 07:30:52 +0530

    refactor: used plot method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+from sklearn.datasets import fetch_california_housing
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 if current_dir not in sys.path:
@@ -10,15 +12,20 @@ from core.analyze import Analyzer
 
 def main():
     analyzer = Analyzer(show_plots=True)
-     
-    from sklearn.datasets import fetch_california_housing
 
     housing = fetch_california_housing(as_frame=True)
     target = housing.frame.columns.to_list()[-1]
     data = housing.frame
     
+    # Analyze the dataset
     result = analyzer.run(data, target)
+    
+    # Plot graphs 
+    analyzer.plot()
+    
     return result
 
 if __name__ == '__main__':
     results = main()
+    
+    print(results["insights"]["summary"])
