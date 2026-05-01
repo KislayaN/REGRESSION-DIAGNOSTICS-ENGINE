9e373f4804b2bb86a2c936ae122077731897b3e4
Kislaya N <kislaya345@gmail.com>
2026-05-01 07:51:12 +0530

    refactor: separated fit and transform methods

--- helpers/handle_missing_values.py
+++ helpers/handle_missing_values.py
@@ -5,9 +5,12 @@ class Imputer:
         self.impute_method = KNNImputer(n_neighbors=5, weights='distance')
         self.transformed_data = None
         
+    def fit(self, target):
+        self.impute_method.fit(target)
+        
     def transform(self, target):
         """The value you want to impute is target"""
         
-        target = self.impute_method.fit_transform(target)
+        target = self.impute_method.transform(target)
         self.transformed_data = target
         return self.transformed_data
